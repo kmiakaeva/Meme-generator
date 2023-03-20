69e5786ded6725763b7877490e1979a206ff7e96
Kristina Miakaeva <92623612+kmiakaeva@users.noreply.github.com>
2023-03-20 13:47:44 +0300

    add random meme image

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,23 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+import memesData from '../memeData';
 
 export default function Meme() {
+  const [meme, setMeme] = useState({
+    topText: '',
+    bottomText: '',
+    randomImage: 'https://i.imgflip.com/23ls.jpg',
+  });
+
+  const [allMemeImages, setAllMemeImages] = useState(memesData);
+
+  function getMemeImage() {
+    const memesArray = allMemeImages.data.memes;
+    const random = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[random].url;
+    setMeme((prevMeme) => {
+      return {
+        ...prevMeme,
+        randomImage: url,
+      };
+    });
+  }
+
   return (
     <main>
-      <form className="form">
+      <div className="form">
         <input
           className="form__text-field"
           type="text"
-          placeholder="Shut up"
+          placeholder="Top Text"
         ></input>
         <input
           className="form__text-field"
           type="text"
-          placeholder="and take my money"
+          placeholder="Bottom Text"
         ></input>
-        <button className="form__submit-btn" type="submit" value="Submit">
+        <button
+          onClick={getMemeImage}
+          className="form__submit-btn"
+          type="submit"
+          value="Submit"
+        >
           Get a new meme image 🖼
         </button>
-      </form>
+      </div>
+      <img className="meme-image" src={meme.randomImage} alt="Meme"></img>
     </main>
   );
 }
